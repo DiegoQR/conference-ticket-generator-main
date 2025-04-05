b0b02aecfa61f531f2518f32a71790cfa998b8e6
DiegoQR <diegoqr2000@gmail.com>
2025-04-04 20:45:39 -0400

    Changed minor misspelling properties

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 function Input(props) {
     const { children, name, ...rest } = props;
     return (<>
-        <label for={name} className='flex flex-col gap-3 text-2xl font-bold'>
+        <label htmlFor={name} className='flex flex-col gap-3 text-2xl font-bold'>
                 <span>{children}</span>
-                <input id={name} name={name} className='w-full border-1 border-neutral-300 rounded-lg p-2 bg-neutral-300/20 hover:bg-neutral-300/40' {...rest}/>
+                <input id={name} name={name} 
+                    className='w-full border-1 border-neutral-300 rounded-lg p-2 bg-neutral-300/20 hover:bg-neutral-300/40' 
+                    {...rest}
+                    />
         </label>
     </>);
 }
