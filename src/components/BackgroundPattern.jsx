b9754862085b6332b950b8bfd130c36a0d2e228a
DiegoQR <diegoqr2000@gmail.com>
2025-04-11 20:28:02 -0400

    Refactor image paths in HTML and JSX files to use absolute URLs for consistency

--- src/components/BackgroundPattern.jsx
+++ src/components/BackgroundPattern.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 function BackgroundPattern() {
     return (<>
         <img id='pattern-lines' 
-            src='./assets/images/pattern-lines.svg' 
+            src='/assets/images/pattern-lines.svg' 
             className='absolute insert-0 -z-10 h-3/4 min-w-full object-cover'
             alt='pattern-lines'
         />
         <img id='squiggly-line'
-            src='./assets/images/pattern-squiggly-line-top.svg' 
+            src='/assets/images/pattern-squiggly-line-top.svg' 
             className='absolute -z-10 top-10 right-0 object-cover max-w-50 lg:max-w-100'
             alt='squiggly-line'
         />
         <img id='circles-1'
-            src='./assets/images/pattern-circle.svg' 
+            src='/assets/images/pattern-circle.svg' 
             className='absolute -z-10 -top-5 -left-7 object-cover max-w-40'
             alt='circles-1'
         />
         <img id='circles-2'
-            src='./assets/images/pattern-circle.svg' 
+            src='/assets/images/pattern-circle.svg' 
             className='absolute -z-10 bottom-50 -right-15 object-cover max-w-30 lg:max-w-500 lg:right-50'
             alt='circles-2'
         />
         <img id='squiggly-line-bot'
-            src='./assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg'
+            src='/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg'
             className='absolute -z-10 bottom-0 left-0 object-cover max-w-100 lg:max-w-150 lg:content-[url("./assets/images/pattern-squiggly-line-bottom-desktop.svg")]'
             alt='squiggly-line-bot'
         />
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,11 +4,11 @@ function Ticket({fullName, username, avatarUrl}) {
 
     return (<>
         <article 
-            className='bg-[url(./assets/images/pattern-ticket.svg)] bg-contain bg-no-repeat w-full max-w-lg mx-auto aspect-15/7 mt-10
+            className='bg-[url(/assets/images/pattern-ticket.svg)] bg-contain bg-no-repeat w-full max-w-lg mx-auto aspect-15/7 mt-10
             grid grid-cols-[85%_15%] grid-rows-2 grid-flow-col p-5 items-center md:gap-y-10'
         >
             <section className='grid grid-cols-[20%_auto] grid-rows-2'>
-                <img src='./assets/images/logo-mark.svg' alt='Logo' className='row-span-2 self-center'></img>
+                <img src='/assets/images/logo-mark.svg' alt='Logo' className='row-span-2 self-center'></img>
                 <h3 className='text-xl font-bold md:text-2xl'>Coding Conf</h3>
                 <span className='text-sm text-neutral-400 font-light md:text-lg'>Jan 31, 2025 / Austin, TX</span>
             </section>
@@ -16,7 +16,7 @@ function Ticket({fullName, username, avatarUrl}) {
                 <img src={avatarUrl} alt='avatar' className='aspect-square w-12 md:w-14 rounded-xl row-span-2 self-center'/>
                 <h3 className='text-xl md:text-2xl'>{fullName}</h3>
                 <div className='flex gap-2 content-center self-center'>
-                    <img src='./assets/images/icon-github.svg' alt='github-logo' className='aspect-square w-5'/>
+                    <img src='/assets/images/icon-github.svg' alt='github-logo' className='aspect-square w-5'/>
                     <span className='text-sm text-neutral-400 font-light md:text-lg'>{username}</span>  
                 </div>   
             </section>
